18833359095c8996bed9c4651de7f81169f5241a
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-08 03:32:00 +0200

    """

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
 """
-Deletes out-of-date archives
-fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
+Deletes out-of-date archives,
+using the function do_clean
 """
-
-import os
 from fabric.api import *
+from datetime import datetime
+import os
 
 env.hosts = ['52.72.74.64', '3.90.80.21']
+env.user = 'ubuntu'
 
 
-def do_clean(number=0):
-    """Delete out-of-date archives.
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+def deploy():
+    ''' Deploys archive '''
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
